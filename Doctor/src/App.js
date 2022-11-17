d32e47908f837606458a3e2b0e5b450017da3434
Chestha Goyal <chesthagoyal0000@gmail.com>
2022-11-17 20:42:45 +0530

    Add files via upload

---
+++ Doctor/src/App.js
@@ -0,0 +1,35 @@
+
+import { BrowserRouter, Route,Routes} from 'react-router-dom';
+import './App.css';
+import Navbar from "./components/Navbar"
+import Header from "./components/hero"
+import About from './components/about';
+import Contact from './components/feedback';
+import Allqueries from './components/Allqueries';
+import Signin from './components/signin';
+import Added from './components/Added';
+import Form from './components/Header';
+
+
+function App() {
+  return (
+
+    <BrowserRouter>
+     <Routes>
+     <Route path="/" element={<><Navbar /> <Header></Header></>}></Route>
+     <Route path="/about" element={<><Navbar /><About/></>}></Route>
+     <Route path="/Contact" element={<><Navbar/> <Contact/></>}></Route>
+      
+      <Route path='/added' element={<Added/>}></Route>
+      <Route path="/admin" element={<><Signin/></>}></Route>
+      
+    
+     </Routes>
+
+    </BrowserRouter>
+
+    
+  );
+}
+
+export default App;
